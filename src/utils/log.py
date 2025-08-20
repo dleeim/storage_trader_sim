30b806c4b37020900014ffed4fd9d487962caf44
dleeim <dgyleeim@gmail.com>
2025-08-20 18:25:55 +0900

    simplified git repo by removing unused files

--- src/utils/log.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-
-logger.remove()
-logger.add(
-    sink=lambda msg: print(msg, end=""),
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{message}</cyan>",
-    level="INFO",
-)
